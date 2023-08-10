340f6d6cfb11035f765a3ae7c992ed36fcf6dc42
zicog <ZiCog@users.noreply.github.com>
2023-08-10 18:06:52 +0300

    Generate reverse look up (sensor to angle) table

--- src/gray_code.rs
+++ src/gray_code.rs
@@ -6,32 +6,47 @@ use bitintr::Popcnt;
 pub fn single_track_gray_code() {
     // A single track Gray code with 9 sensors spaced at 40 degreees, 1 degre steps.
     const SingleTrack: &str= "001100000000000000000011111100111111100000011111000000011111000111111110011100000000000111100111001111110000011111100000000000000011110001111111111111111100000000000000000011111111100001100000000000000000000000000000000111111111111111111100011111000000000000000000000000001111111000000111100000000000000000111111111111111111111111111111111111111111111111111111";
-
+    const TrackLength: usize = 360;
     let single_track = SingleTrack.as_bytes();
-    assert_eq!(single_track.len(), 360);
+    assert_eq!(single_track.len(), TrackLength);
 
     // Construct sensor output look up table from Gray code sequence.
-    let mut output_table: Vec<u64> = vec![];
-    for angle in 0..360 {
-        let mut output = 0u64;
+    let mut output_table: Vec<u16> = vec![];
+    for angle in 0..TrackLength {
+        let mut output = 0u16;
         for sensor in 0..9 {
-            let bit = single_track[(angle + sensor * 40) % 360];
+            let bit = single_track[(angle + sensor * 40) % TrackLength];
             let bit = if bit == b'1' { 1 } else { 0 };
             output = output | (bit << sensor)
         }
         output_table.push(output);
     }
-    // Verify only 1 bit changes for each step of output table and print it
-    // Nine bit single track gray code output.
-    println!("Angle : Output");
+    // Verify only 1 bit changes for each step of output table and print
     for a in 0..output_table.len() {
-        println!("{:03}   : {:09b}", a, output_table[a]);
-        let changed_bits = output_table[a] ^ output_table[(a + 1) % output_table.len()];
+        let changed_bits = output_table[a] ^ output_table[(a + 1) % TrackLength];
         assert_eq!(changed_bits.popcnt(), 1);
     }
+
+    // Generate the input table (revese look up, sensor to angle)
+    let mut input_table: Vec<Option<u16>> = vec![None; 512];
+    for a in 0..output_table.len() {
+        input_table[output_table[a as usize] as usize] = Some(a as u16);
+    }
+    // Print the ouput table
+    println!("Angle : output");
+    for a in 0..output_table.len() {
+        println!("{} : {:?}", a, output_table[a]);
+    }
+
+    // Print the input table
+    println!("Input : angle");
+    for i in 0..input_table.len() {
+        println!("{} : {:?}", i, input_table[i]);
+    }
+
     // Verfy no duplicate codes in outout table.
     output_table.sort();
     for a in 0..output_table.len() {
-        assert_ne!(output_table[a], output_table[(a + 1) % output_table.len()]);
+        assert_ne!(output_table[a], output_table[(a + 1) % TrackLength]);
     }
 }
